90804cbdb994636a034f8df27eca15ff2762edb9
eulhch <129867999+eulhch@users.noreply.github.com>
2023-04-19 14:38:02 +0200

    Update streamlit_app.py
    add requests json

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,13 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
